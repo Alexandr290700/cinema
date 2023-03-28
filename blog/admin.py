cd45a824ebe63ee784c80850e58fa07cf6d658c4
sashawhite@alex <alexandrkim.297@gmail.com>
2023-03-28 12:26:35 +0600

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,78 @@
+from django.contrib import admin
+from .models import Movie, Saloon, Seans, JobTitle, Places, SectorSaloon, Employees, PriceForTickets, Tickets, MovingTickets
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'duration', 'rental_start_date', 'rental_finish_date', 'sales_company')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'duration')
+    list_filter = ('id', 'rental_start_date')
+
+class SaloonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'count_place', 'description', 'number_of_rows', 'number_of_places')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', )
+    list_filter = ('id', 'count_place')
+
+class SeansAdmin(admin.ModelAdmin):
+    list_display = ('id', 'saloon', 'date', 'time', 'movie')
+    list_display_links = ('id', 'saloon')
+    search_fields = ('saloon', )
+    list_filter = ('saloon', 'movie')
+
+class JobTitleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', )
+    list_filter = ('id', )
+
+class PlacesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'saloon', 'row_number', 'row_place')
+    list_display_links = ('id', 'saloon')
+    search_fields = ('saloon', )
+    list_filter = ('id', 'saloon')
+
+class SectorSaloonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'saloon', 'name', 'description')
+    list_display_links = ('id', 'saloon', 'name')
+    search_fields = ('saloon', 'name')
+    list_filter = ('id', 'saloon', 'name')
+
+class EmployeesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'title', 'password')
+    list_display_links = ('id', 'name', 'title')
+    search_fields = ('name', 'title')
+    list_filter = ('id', 'name', 'title')
+
+class PriceForTicketsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'seanse', 'sector', 'price')
+    list_display_links = ('id', 'seanse', 'sector')
+    search_fields = ('seanses', 'sector')
+    list_filter = ('id', 'seanse', 'sector')
+
+class TicketsAdmin(admin.ModelAdmin):
+    list_display = ('ticket_number', 'date_created', 'seanses', 'places', 'played', 'booking', 'creashed')
+    list_display_links = ('ticket_number', 'date_created', 'seanses')
+    search_fields = ('seanses', 'ticket_number')
+    list_filter = ('ticket_number', 'date_created', 'seanses')
+
+class MovingTicketsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'moving_tickets', 'ticket', 'date_create', 'operation', 'employee')
+    list_display_links = ('id', 'moving_tickets','ticket', 'employee')
+    search_fields = ('ticket', 'employee')
+    list_filter = ('id', 'ticket')
+    
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Saloon, SaloonAdmin)
+admin.site.register(Seans, SeansAdmin)
+admin.site.register(JobTitle, JobTitleAdmin)
+admin.site.register(Places, PlacesAdmin)
+admin.site.register(SectorSaloon, SectorSaloonAdmin)
+admin.site.register(Employees, EmployeesAdmin)
+admin.site.register(PriceForTickets, PriceForTicketsAdmin)
+admin.site.register(Tickets, TicketsAdmin)
+admin.site.register(MovingTickets, MovingTicketsAdmin)
+
+
+    
---
+++ blog/models.py
@@ -0,0 +1,137 @@
+from django.db import models
+
+# Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название фильма')
+    duration = models.CharField(max_length=50, verbose_name='Длительность')
+    rental_start_date = models.DateField(verbose_name='Дата начала проката')
+    rental_finish_date = models.DateField(verbose_name='Дата окончания проката')
+    sales_company = models.CharField(max_length=255, verbose_name='Кинокомпания')
+
+    class Meta:
+        verbose_name = 'Фильм'
+        verbose_name_plural = 'Фильмы'
+        ordering = ['name']
+
+    def __str__(self) -> str:
+        return self.name
+
+class Saloon(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название зала')
+    count_place = models.IntegerField(verbose_name='Количество мест')
+    description = models.CharField(max_length=50, verbose_name='Описание')
+    number_of_rows = models.IntegerField(verbose_name='Номер ряда')
+    number_of_places = models.IntegerField(verbose_name='Номер места')
+
+    class Meta:
+        verbose_name = 'Зал'
+        verbose_name_plural = 'Залы'
+        ordering = ['name']
+
+    def __str__(self):
+        return str(self.name)
+
+class Seans(models.Model):
+    saloon = models.ForeignKey(Saloon, on_delete=models.CASCADE, verbose_name='Зал')
+    date = models.DateField(verbose_name='Дата')
+    time = models.TimeField(verbose_name='Время')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, verbose_name='Фильм')
+
+    class Meta:
+        verbose_name = 'Сеанс'
+        verbose_name_plural = 'Сеансы'
+        ordering = ['movie']
+
+    def __str__(self):
+        return str(self.saloon)
+
+class JobTitle(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Должность')
+
+    class Meta:
+        verbose_name = 'Должность'
+        verbose_name_plural = 'Должности'
+        ordering = ['title']
+    
+    def __str__(self) -> str:
+        return self.title
+
+class Places(models.Model):
+    saloon = models.ForeignKey(Saloon, on_delete=models.CASCADE, verbose_name='Зал')
+    row_number = models.IntegerField(verbose_name='Номер ряда')
+    row_place = models.IntegerField(verbose_name='Номер места')
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
+        ordering = ['saloon']
+
+    def __str__(self) -> str:
+        return str(self.saloon)
+
+class SectorSaloon(models.Model):
+    saloon = models.ForeignKey(Saloon, on_delete=models.CASCADE, verbose_name='Зал')
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.CharField(max_length=50, verbose_name='Описание')
+
+    class Meta:
+        verbose_name = 'Сектор'
+        verbose_name_plural = 'Сектора'
+        ordering = ['name']
+
+    def __str__(self) -> str:
+        return self.name
+
+class Employees(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    title = models.ForeignKey(JobTitle, on_delete=models.CASCADE,max_length=40, verbose_name='Должность')
+    password = models.CharField(max_length=100, verbose_name='Пароль')
+
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
+        ordering = ['name']
+
+    def __str__(self) -> str:
+        return self.name
+
+class PriceForTickets(models.Model):
+    seanse = models.ForeignKey(Seans, on_delete=models.CASCADE, verbose_name='Сеансы')
+    sector = models.ForeignKey(SectorSaloon, on_delete=models.CASCADE, verbose_name='Сектор')
+    price = models.IntegerField(verbose_name='Цена')
+
+    class Meta:
+        verbose_name = 'Цена билета'
+        verbose_name_plural = 'Цена билетов'
+        ordering = ['seanse']
+
+class Tickets(models.Model):
+    ticket_number = models.IntegerField(verbose_name='Номер билета')
+    date_created = models.DateField(verbose_name='Дата')
+    seanses = models.ForeignKey(Seans, on_delete=models.CASCADE, verbose_name='Сеанс')
+    places = models.ForeignKey(Places, on_delete=models.CASCADE, verbose_name='Место')
+    played = models.BooleanField(verbose_name='Оплачено')
+    booking = models.BooleanField(verbose_name='Бронь')
+    creashed = models.BooleanField(verbose_name='creashed')
+
+    class Meta:
+        verbose_name = 'Билет'
+        verbose_name_plural = 'Билеты'
+        ordering = ['seanses']
+
+    def __str__(self) -> str:
+        return str(self.ticket_number)
+    
+
+class MovingTickets(models.Model):
+    moving_tickets = models.IntegerField(verbose_name="Движение билетов")
+    ticket = models.ForeignKey(Tickets, on_delete=models.CASCADE, verbose_name='Номер билета')
+    date_create = models.DateField(verbose_name='Дата')
+    operation = models.CharField(max_length=50, verbose_name='Операция')
+    employee = models.ForeignKey(Employees, on_delete=models.CASCADE, verbose_name='Сотрудник')
+
+    class Meta:
+        verbose_name = 'Движение билета'
+        verbose_name_plural = 'Движение билетов'
+        ordering = ['ticket']
